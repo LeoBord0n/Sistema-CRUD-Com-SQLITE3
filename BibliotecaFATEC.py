a58029ca2f7465dc9bcd200936c7a726e9048f8c
Leonardo Bordon <141795876+LeoBord0n@users.noreply.github.com>
2023-08-23 00:56:05 -0300

    BibliotecaFATEC.py

--- BibliotecaFATEC.py
+++ BibliotecaFATEC.py
@@ -3,18 +3,23 @@ import sqlite3
 
 biblioteca = sqlite3.connect('biblioteca.db')
 cursor = biblioteca.cursor()
-#cursor.execute(f'SELECT * FROM {biblioteca}')
-# for row in cursor.fetchall():
-#     print(row)
+cursor.execute(f'SELECT * FROM {biblioteca}')
+ for row in cursor.fetchall():
+     print(row)
+     
 #     'aluno', 'CPF', 'E-mail', 'Livro alocado', 'Prazo de devoução' 
-#cursor.execute("CREATE TABLE aluno(Aluno text, CPF integer, 'E-mail' text, 'Livro alocado' text, 'Prazo de devolução' text)")
-#sql = (
-    #f'INSERT INTO aluno VALUES(?,?,?,?,?)')
-#cursor.execute(sql,('José','54897681232','josé.felipe@fatec.sp.gov.br','A ordem da IA','24/08/2023'))
-#cursor.execute("DELETE from aluno WHERE Aluno = '1'")
-#cursor.execute("UPDATE Aluno set CPF = 51184404836 where aluno = 'Leonardo'")
-#biblioteca.commit()
+cursor.execute("CREATE TABLE aluno(Aluno text, CPF integer, 'E-mail' text, 'Livro alocado' text, 'Prazo de devolução' text)")
+
+sql = (
+    f'INSERT INTO aluno VALUES(?,?,?,?,?)')
+cursor.execute(sql,('José','54897681232','josé.felipe@fatec.sp.gov.br','A ordem da IA','24/08/2023'))
+
+cursor.execute("DELETE from aluno WHERE Aluno = '1'")
+
+cursor.execute("UPDATE Aluno set CPF = 51184404836 where aluno = 'Leonardo'")
+
+biblioteca.commit()
 cursor.close()
-#biblioteca.close()
+biblioteca.close()
 
 
